0776348d9400f528c8b00cdaa539e534fab264f3
SantiagoDemattei <sdemattei@frba.utn.edu.ar>
2022-08-13 15:39:51 -0300

    asd

--- src/main/java/CRUDs/CRUDUsuario.java
+++ src/main/java/CRUDs/CRUDUsuario.java
@@ -1,4 +1,16 @@
 package CRUDs;
 
+
+import
+import org.hibernate.Session;
+
 public class CRUDUsuario {
+
+    public void insertarUsuario(Usuario u){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+
+    }
+
 }
