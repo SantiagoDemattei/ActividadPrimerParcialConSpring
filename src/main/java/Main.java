45555f1cbf82e7222808fc7d270276c10ff117a5
SantiagoDemattei <sdemattei@frba.utn.edu.ar>
2022-08-13 15:40:00 -0300

    Changes by dani

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,42 +9,15 @@ import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
 public class Main {
-    private final static Logger log = Logger.getLogger(Main.class);
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-        Logger.getLogger("org.hibernate").setLevel(Level.WARN);
-        new Main();
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/parcialdb?useSSL=false";
+        String username = "admin";
+        String password = "admin";
+
     }
 
-    public Main() {
-        createAndStoreEvent("El Hibernate.Event", new Date());
-        listEvents();
-        HibernateUtil.getSessionFactory().close();
-    }
 
-    private Long createAndStoreEvent(String title, Date theDate) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Event theEvent = new Event();
-        theEvent.setTitle(title);
-        theEvent.setDate(theDate);
-        session.save(theEvent);
-        session.getTransaction().commit();
-        log.info("Insertado: "+theEvent);
-        return theEvent.getId();
-    }
 
-    private List<Event> listEvents() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        List<Event> result = (List<Event>)session.createQuery("from Event").list();
-        session.getTransaction().commit();
-        for (Event evento : result) {
-            log.info("Leido: "+evento);
-        }
-        return result;
-    }
 }
